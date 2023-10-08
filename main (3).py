9a07a3e30864ebbcb85c6ab12a181ffac9f2cd1b
shajithajameema <144404378+shajithajameema@users.noreply.github.com>
2023-10-08 19:49:40 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,24 @@
+class Player:
+
+  def play(self):
+    print("The player is playingcricket.")
+
+
+class Batsman(Player):
+
+  def play(self):
+    print("The batsman is batting.")
+
+
+class Bowler(Player):
+
+  def play(self):
+    print("The bowler is bowling.")
+
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+# Call the Play method for each objects
+batsman.play()
+bowler.play()
